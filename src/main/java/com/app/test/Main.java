cf69d68c55754bd83ae1c15d87b6676babd633c3
kirilinko <franckivond@gmail.com>
2025-07-08 16:16:54 +0200

    update

--- src/main/java/com/app/test/Main.java
+++ src/main/java/com/app/test/Main.java
@@ -1,17 +1,8 @@
-package userapp;
+package test;
 
-import userapp.input.UserInputHandler;
-import userapp.database.DatabaseManager;
 
 public class Main {
     public static void main(String[] args) {
-        UserInputHandler inputHandler = new UserInputHandler();
-        String username = inputHandler.askForUsername();
-
         
-        String whereClause = inputHandler.formatCondition(username);
-
-        DatabaseManager db = new DatabaseManager();
-        db.getUserInfo(whereClause);
     }
 }
