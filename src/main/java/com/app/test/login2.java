5394af4637faf531fe927a1d578f81f13c199320
kirilinko <franckivond@gmail.com>
2025-07-08 14:58:53 +0200

    update

---
+++ src/main/java/com/app/test/login2.java
@@ -0,0 +1,35 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class Login{
+
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/mydb";  
+        String userDB = "root";
+        String passwordDB = "784dss";
+
+        try (Connection conn = DriverManager.getConnection(url, userDB, passwordDB);
+             Statement stmt = conn.createStatement()) {
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Entrez votre nom d'utilisateur : ");
+            String username = scanner.nextLine();
+            System.out.print("Entrez votre mot de passe : ");
+            String password = scanner.nextLine();
+
+           
+            String query = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'";
+ 
+            ResultSet rs = stmt.executeQuery(query);
+
+            if (rs.next()) {
+                System.out.println("Connexion réussie !");
+            } else {
+                System.out.println("Échec de connexion.");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
